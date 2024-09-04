2d21e7a5ef76c0fbacd4c280d2924a5055c5edaf
BiaCarvalhoCavalieri <bia3003carvalho@gmail.com>
2024-09-04 00:06:05 -0300

    fix: layout container height adjustments

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -3,8 +3,8 @@ import styled from 'styled-components'
 export const LayoutContainer = styled.div`
   max-width: 74rem;
   width: calc(100% - 4rem);
-  height: calc(100vh - 10rem);
-  margin: 5rem auto;
+  height: 100%;
+  margin: 2.5rem auto;
   padding: 2.5rem;
   background-color: ${(props) => props.theme['gray-800']};
   border-radius: 0.5rem;
